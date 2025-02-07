d506539eebdd16aafb449d8174c69a0445cd1973
Adam Lay <adam93.lay+git@gmail.com>
2025-02-07 21:53:22 +0000

    Dark mode

--- src/theme.ts
+++ src/theme.ts
@@ -1,8 +1,10 @@
 import { createTheme } from "@mui/material";
 
 export const theme = createTheme({
+  colorSchemes: {
+    dark: true,
+  },
   typography: {
     fontFamily: '"Source Sans 3", Arial, Helvetica, sans-serif !important',
   },
-
 });
